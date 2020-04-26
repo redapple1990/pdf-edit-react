d7b36cb6967d4f2adfdeeb88f0ae2c8f10cb60c1
Cool Engineer <55289773+coolenginner@users.noreply.github.com>
2020-04-26 04:41:25 +0300

    upload

--- src/App.js
+++ src/App.js
@@ -1,115 +1,115 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
 import { Container, Header, Grid, Form, Popup } from 'semantic-ui-react';
 import { Document, Page } from 'react-pdf';
-import Homepreview from './components/Homepreview';
 import '../src/assets/styles.css'
-import Draggable, { ControlPosition } from 'react-draggable';
-import Popupsignature from './components/Dragsidebar';
+import { Draggable, Droppable } from "react-drag-and-drop";
+import { Rnd } from 'react-rnd'
+
 import * as jsPDF from 'jspdf';
 import * as html2canvas from 'html2canvas';
 import styles from './assets/styles.css'
 import $ from 'jquery';
 import SignaturePad from 'react-signature-canvas'
+import { v4 as uuid } from "uuid";
 
+import { pdfjs } from 'react-pdf';
 
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-class App extends React.Component {
+function App(props) {
+  const [file, setFile] = useState(null);
+  const [numPages, SetNumPages] = useState(0);
+  const [pageNumber, SetPageNumber] = useState(1);
+  const [activeDrags, setActiveDrags] = useState(0);
+  const [trimmedDataURL, setTrimmedDataURL] = useState(null);
+  const [usertext, setUsername] = useState();
+  const [date, setDate] = useState();
+  const [showtext, toggleShowtext] = useState(false);
+  const [showPopup, setShowPopup] = useState(false);
+  const [showDate, setShowDate] = useState(true);
 
-  state = {
-    file: null,
-    numPages: 0,
-    pageNumber: 1,
-    activeDrags: 0,
-    trimmedDataURL: null,
-    dates: null,
-    username: '',
-    date: ""
-  };
+  useEffect(() => {
+    useDate();
+  })
 
-  componentDidMount() {
-    this.date();
-  }
-
-  date(){
+  const useDate = () => {
     var date = new Date().toDateString();
-    this.setState({ date });
+    setDate(date);
+
   }
 
-  mySubmitHandler = (event) => {
+  const toggleDate = () => {
+    setShowDate(!showDate);
+  }
+
+  const mySubmitHandler = (event) => {
     event.preventDefault();
   }
-  myChangeHandler = (event) => {
-    this.setState({ username: event.target.value });
+
+  const myChangeHandler = (event) => {
+    setUsername(event.target.value);
+
   }
 
-  togglText() {
-    this.setState({
-      showtext: !this.state.showtext
-    });
-  }
+  const toggleText = (e) => {
+    toggleShowtext(!showtext)
 
-  sigPad = {}
-  clear = () => {
-    this.sigPad.clear()
-  }
-  trim = () => {
-    this.setState({
-      trimmedDataURL: this.sigPad.getTrimmedCanvas()
-        .toDataURL('image/png')
-    })
-  }
-
-  togglePopup() {
-    this.setState({
-      showPopup: !this.state.showPopup
-    });
-  }
-  toggleDate() {
-    this.setState({
-      showDate: !this.state.showDate
-    });
-   
-}
-  handleDrag = (e, ui) => {
-    const { x, y } = this.state.deltaPosition;
-    this.setState({
-      deltaPosition: {
-        x: x + ui.deltaX,
-        y: y + ui.deltaY,
-      }
-    });
   };
 
-  onFileChange = (event) => {
-    this.setState({
-      file: event.target.files[0]
-    });
+
+
+
+  var sigPad = {}
+
+  const clear = () => {
+    sigPad.clear()
   }
 
-  onDocumentLoadSuccess = ({ numPages }) => {
-    this.setState({ numPages });
+  const trim = () => {
+
+    setTrimmedDataURL(
+      sigPad.getTrimmedCanvas()
+        .toDataURL('image/png')
+    )
+
+    setShowPopup(!showPopup);
   }
 
-  nextPage = () => {
 
-    const currentPageNumber = this.state.pageNumber;
+  const togglePopup = () => {
+    if (trimmedDataURL === null) {
+      setShowPopup(!showPopup);
+    }
+  }
+
+
+  const onFileChange = (event) => {
+    setFile(event.target.files[0])
+    console.log(file);
+  }
+
+
+  const onDocumentLoadSuccess = (numPages) => {
+    SetNumPages(numPages);
+  }
+
+  const nextPage = () => {
+    const currentPageNumber = SetPageNumber.pageNumber;
     let nextPageNumber;
 
-    if (currentPageNumber + 1 > this.state.numPages) {
+    if (currentPageNumber + 1 > SetNumPages.numPages) {
       nextPageNumber = 1;
     } else {
       nextPageNumber = currentPageNumber + 1;
     }
-
-    this.setState({
-      pageNumber: nextPageNumber
-    });
+    SetPageNumber(nextPageNumber);
   }
-  handlePdf() {
+
+  const handlePdf = () => {
     var canvas_data = document.getElementsByTagName("canvas");
     var w = canvas_data.item(0).width
     var h = canvas_data.item(0).height
-
 
     var w = $(".canvas_div_pdf").width();
     var h = $(".canvas_div_pdf").height();
@@ -124,9 +124,9 @@ class App extends React.Component {
 
     html2canvas($("#page")[0], { allowTaint: true }).then(function (canvas) {
       canvas.getContext('2d');
-      console.log(canvas.height + "  " + canvas.width);
+      console.log(canvas.height + " " + canvas.width);
 
-      var imgData = canvas.toDataURL("image/jpeg");
+      var imgData = canvas.toDataURL("image/jpeg",1.0);
       var pdf = new jsPDF('p', 'pt', [PDF_Width, PDF_Height]);
       pdf.addImage(imgData, 'JPG', 0, 0, canvas_image_width, canvas_image_height);
 
@@ -137,142 +137,216 @@ class App extends React.Component {
 
       pdf.save("HTML-Document.pdf");
     });
-  };
+
+  }
 
 
-  render() {
-    const { pageNumber, numPages, PDFWidth,date } = this.state;
-    const dragHandlers = { onStart: this.onStart, onStop: this.onStop };
-    return (
+
+
+  const [dragitem, setDragitem] = React.useState([])
+
+
+  const onDrop = React.useCallback(
+    (result) => {
+      let newArray = [...dragitem]
+      newArray.push(result)
+      setDragitem(newArray)
+    },
+    console.log(dragitem)
+
+  );
+
+  function handleDrag(ui, index) {
+    let item = dragitem
+    console.log(dragitem);
+  }
+
+  return (
+    <div>
+
       <div>
 
-        {/* <div class="sidenav">
-          <a href="#about">About</a>
-         
-        </div> */}
-
-        <div class="main">
-        <main id="page-content-wrapper page" role="main" className="container">
-          <Header textAlign="center">PDF Preview</Header>
-          <button onClick={this.handlePdf} className="btn btn-primary btn-lg mx-auto">Generate PDF</button>
-
-          <div>
-            <Form>
-              <div className="upload-btn">
-                <input type="file" onChange={this.onFileChange} className="button-select">
-                </input>
-              </div>
-            </Form>
-
-            <div className="pagination-item" >
-              <Grid centered columns={2}>
-                <Grid.Column textAlign="center" onClick={this.nextPage}>
-                  {this.state.file ? <div class="pagination">
-                    <a href="#">&laquo;</a>
-                    <a href="#" class="active">{pageNumber}</a>
-                    <a href="#">of</a>
-                    <a href="#">{numPages}</a>
-                    <a href="#">&raquo;</a>
-                  </div> : null}
-                </Grid.Column>
-              </Grid>
-            </div>
-
-            <div id="page">
-              <Draggable handle="strong" >
-                <div className="box no-cursor " >
-                  <div className="canvas_div_pdf">
-                    <Document file={this.state.file} onLoadSuccess={this.onDocumentLoadSuccess}
-                      noData={<h4 className="success-file" >Please select a file</h4>} >
-
-                      <Page pageNumber={pageNumber} width={PDFWidth} />
-                    </Document>
-
-
-                  </div>
-                </div>
-              </Draggable>
-              <div className="add-sidebar">
-                <div className="drag-list">
-                  <Draggable cancel="strong" {...dragHandlers}   >
-                    <div className={this.state.trimmedDataURL ? "drag-item-box-signture" : "drag-item-box"} onClick={this.togglePopup.bind(this)}>
-                      {this.state.trimmedDataURL
-                        ? <img className={styles.sigImage}
-                          src={this.state.trimmedDataURL} />
-                        : (<div>Signature</div>)}
-                    </div>
-                  </Draggable>
-                  {this.state.showPopup &&
-              <div className="Apps">
-                <div className={styles.container}>
-                  <div className={styles.sigContainer}>
-                    <SignaturePad canvasProps={{ className: styles.sigPad }}
-                      ref={(ref) => { this.sigPad = ref }} />
-                  </div>
-                  <div>
-                    <button className={styles.buttons} onClick={this.clear}>
-                      Clear  </button>
-                    <button className={styles.buttons} onClick={this.trim}>
-                      Save</button>
-                    <button onClick={this.props.closePopup}>
-                      Close</button>
-                  </div>
-                </div>
-              </div>
-            }
-
-          
-
-                </div>
-                <div className="drag-list">
-                  <Draggable cancel="strong" {...dragHandlers}   >
-                    <div className={this.state.username ? "drag-item-box-signture" : "drag-item-box"} onClick={this.togglText.bind(this)}>
-                      {this.state.username
-                        ? <h1 className="date-color">{this.state.username}</h1>
-                        : (<div>Initials</div>)}
-                    </div>
-                  </Draggable>
-                  {this.state.showtext &&
-              <div className="Apps">
-                <form onSubmit={this.mySubmitHandler}>
-                  <input
-                    type='text'
-                    onChange={this.myChangeHandler}
-                  />
-                  <input
-                    type='submit'
-                  />
-                </form>
-              </div>
-            }
-                </div><div className="drag-list">
-                  <Draggable cancel="strong" {...dragHandlers}>
-                    <div className={this.state.showDate ? "drag-item-box-signture" : "drag-item-box"} onClick={this.toggleDate.bind(this)}>
-                      {this.state.showDate
-                        ?
-                      <div className="date-color">{date}</div>
-
-                        : (<div>Date</div>)}
-                    </div>
-                  </Draggable>
-                </div>
-              </div>
-            </div>
-
-           
-
-
-          </div>
-{/* <Popupsignature></Popupsignature> */}
-
-        </main>
+        <div className="sidenav">
+          <Draggable type="dragitems" data="Signature"><a>Signature</a></Draggable>
+          <Draggable type="dragitems" data="Intital"><a>Initials</a></Draggable>
+          <Draggable type="dragitems" data="date"><a>Date</a></Draggable>
         </div>
 
-       
+        <div className="main" >
+
+          <Droppable
+            types={["dragitems"]}
+            onDrop={onDrop}>
+            <div className="add-background">
+
+              <button
+                onClick={handlePdf}
+                className="btn btn-primary btn-lg mx-auto"> Generate PDF </button>
+
+              {/* all-popup */}
+
+              {showPopup && (
+                <div className="Apps">
+                  <div>
+                    <div>
+                      <SignaturePad
+                        canvasProps={{
+                          width: 500,
+                          height: 200,
+                          className: "sigCanvas",
+                        }}
+                        ref={(ref) => {
+                          sigPad = ref;
+                        }}
+                      />
+                    </div>
+                    <div>
+                      <button onClick={clear}>Clear </button>
+                      <button onClick={trim}>Save</button>
+                    </div>
+                  </div>
+                </div>
+              )}
+
+
+              {showtext && (
+                <div className="Apps">
+                  <form>
+                    <input type="text" onChange={(e) => myChangeHandler(e)} />
+
+                    <input type="submit" onClick={() => toggleText()} />
+
+                  </form>
+                </div>
+              )}
+
+
+              <Form>
+
+                <div className="upload-btn">
+                  <input type="file" onChange={onFileChange} className="button-select">
+                  </input>
+                </div>
+
+              </Form>
+
+              {/* <div className="pagination-item" >
+                <Grid centered columns={2}>
+                  <Grid.Column textAlign="center" onClick={nextPage}>
+                    {file ? <div class="pagination">
+                      <a href="#">&laquo;</a>
+                      <a href="#" class="active">{pageNumber}</a>
+                      <a href="#">of</a>
+                      <a href="#">{numPages}</a>
+                      <a href="#">&raquo;</a>
+                    </div> : null}
+                  </Grid.Column>
+                </Grid>
+              </div> */}
+                <div className="container">
+
+              <div className="canvas_div_pdf" id="page">
+                  <Document file={file} onLoadSuccess={onDocumentLoadSuccess}
+                    noData={<h4 className="success-file" >Please select a file</h4>}>
+                    <Page pageNumber={pageNumber} />
+                  </Document>
+
+
+                {dragitem.map(item => {
+                  return (
+                    <>
+                      <Rnd
+                        onDragStop={(e, ui) => handleDrag(ui)}>
+
+
+                        {/* signature */}
+
+
+
+                        {item.dragitems == "Signature" ? (
+
+                          <div onClick={() => togglePopup()}>
+                            <div>
+                              {trimmedDataURL
+
+                                ? <img className={styles.sigImage}
+
+                                  src={trimmedDataURL} />
+
+                                : (<div className="items"> {item.dragitems} </div>)}
+                            </div>
+
+                          </div>
+
+                        ) : null}
+
+
+
+                        {item.dragitems == "Intital" && (
+                          <div onClick={() => toggleText()}>
+                            <div>
+
+                              <div className="items" >
+                                {usertext ? usertext : item.dragitems}
+                              </div>
+
+                            </div>
+                          </div>
+                        )}
+
+
+
+                        {item.dragitems == "date" && (
+                          <div onClick={() => toggleDate()}>
+                            <div>
+                              {showDate ? (
+                                <div className="items" >
+                                  {date}
+                                </div>
+                              ) : (
+                                  <div className="items" >{item.dragitems}</div>
+                                )}
+                            </div>
+                          </div>
+                        )}
+
+
+
+                      </Rnd>
+                    </>)
+
+                }
+                )}
+
+              </div>
+
+
+              </div>
+
+
+            </div>
+          </Droppable>
+        </div>
+
       </div>
 
-    );
-  }
+
+
+
+
+
+
+
+
+
+
+    </div>
+
+  );
+
 }
 
-export default App;
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
+
